9fb4469481770814feb733a8551f527ad97d9f54
gonzalo00913 <gonzalo-masa@outlook.com>
2023-03-03 22:11:39 -0300

    agregando estilos y contenido

--- code.js
+++ code.js
@@ -1,4 +1,4 @@
-const navLinks = document.querySelectorAll("#myNav a");
+ const navLinks = document.querySelectorAll("#myNav a");
 
 navLinks.forEach((link) => {
   link.addEventListener("click", function (event) {
@@ -11,6 +11,30 @@ navLinks.forEach((link) => {
     currentActiveSection.style.display = "none";
     section.style.display = "block";
   });
-});
+}); 
+
+
 
 /*********************************************************/
+
+const myName = document.querySelector('.my-name');
+
+
+const myNameText = myName.textContent;
+myName.textContent = '';
+
+let i = 0;
+
+function typeEffect() {
+  if (i < myNameText.length) {
+    myName.textContent += myNameText.charAt(i);
+    i++;
+    setTimeout(typeEffect, 100); 
+  }
+}
+
+typeEffect();
+
+const aboutSection = document.getElementById("about");
+aboutSection.style.display = "block";
+aboutSection.style.opacity = "1";
